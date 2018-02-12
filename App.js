76aa484d5e619d6b8fa4617b9c01085cda9b272c
muralimgwl <murali.m@goodworklabs.com>
2018-02-12 12:41:18 +0530

    Added react native elements and map

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Button >test</Button>
       </View>
     );
   }
